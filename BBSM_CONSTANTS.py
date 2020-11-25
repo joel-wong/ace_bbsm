82f3cae4a66376cdc1a01cbca98172c42f617999
Wenjia Yang <wendyyang0811@gmail.com>
2020-11-24 21:41:10 -0700

    added comment to BBSM_CONSTANTS

--- BBSM_CONSTANTS.py
+++ BBSM_CONSTANTS.py
@@ -3,4 +3,4 @@ PORT = 65432  # Port to listen on, this can be any arbitrary private port betwee
 RECEIVE_BUFFER_SIZE = 4096  # Max receive buffer size (in bytes) to be received over the socket
 MESSAGE_PREFIX_LENGTH = 4  # Number of bytes prefixing each message to indicate the message length
 ALLOW_SERVER_TIMEOUT = False  # implements server timeout
-SERVER_TIMEOUT = 20  # if no client connects within this timeframe (seconds), server closes
+SERVER_TIMEOUT = 20  # If no client connects within this timeframe (seconds), server closes. Ignored if ALLOW_SERVER_TIMEOUT = False
