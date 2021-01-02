8dd113aa9446588dd8f40def7f27707c049d5df2
Joel Wong <joel.wong@ucalgary.ca>
2021-01-01 19:31:47 -0600

    Improve commenting, handle input errors

--- message.py
+++ message.py
@@ -12,10 +12,18 @@ def recv_msg(sock):
     socket_input_file_handle = sock.makefile('r')
     first_input = socket_input_file_handle.readline()
     if first_input != "":
-        # socket has been closed
-        msg_len = int(first_input)
-        msg = socket_input_file_handle.read(msg_len)
+        try:
+            msg_len = int(first_input)
+            msg = socket_input_file_handle.read(msg_len)
+        except ValueError:
+            # expected the length of the msg to be the first_input, but did
+            # not receive an integer value
+            # return an empty string, indicating that the connection should
+            # be terminated
+            msg = ""
     else:
+        # socket has been closed
+        # connection should be terminated
         msg = ""
     socket_input_file_handle.close()
     # Return full message in String form
