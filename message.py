49f80e37dbeb6bb33e851070ee2af9ec6d9683d5
Joel Wong <joel.wong@ucalgary.ca>
2021-01-01 19:03:43 -0600

    More simplications for sending the size of the data

--- message.py
+++ message.py
@@ -3,24 +3,14 @@ import BBSM_CONSTANTS
 
 def send_msg(sock, msg):
     msg_len = len(msg)
-    encoded_msg_len = msg_len.to_bytes(BBSM_CONSTANTS.MESSAGE_PREFIX_LENGTH, 'big')
-    # Send message length
-    sent = sock.send(encoded_msg_len)
-    if sent != BBSM_CONSTANTS.MESSAGE_PREFIX_LENGTH:
-        raise RuntimeError("sending error, not all packets sent")
     socket_output_file_handle = sock.makefile('w')
-    socket_output_file_handle.write(msg)
+    socket_output_file_handle.write(str(msg_len) + "\n" + msg)
     socket_output_file_handle.close()
 
 
 def recv_msg(sock):
-    # Read message length and unpack it into an integer
-    raw_msglen = sock.recv(BBSM_CONSTANTS.MESSAGE_PREFIX_LENGTH)
-    if not raw_msglen:
-        return None
-    msg_len = int.from_bytes(raw_msglen, 'big')
-    # Receive the number of bytes indicated by the message length
     socket_input_file_handle = sock.makefile('r')
+    msg_len = int(socket_input_file_handle.readline())
     msg = socket_input_file_handle.read(msg_len)
     socket_input_file_handle.close()
     # Return full message in String form
