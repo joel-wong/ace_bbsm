1f9c882e467518ad5be9ec3da43b76d10aa39c06
Joel Wong <joelw321@gmail.com>
2021-04-09 01:16:11 +0000

    Merged in joel/update_host_ip (pull request #5)
    * Update host IP address
    * Add comments about HOST and cleanup other comments in BBSM_CONSTANTS
    
    Approved-by: benjamin.forseth
    Approved-by: Taylor Wong

--- BBSM_CONSTANTS.py
+++ BBSM_CONSTANTS.py
@@ -1,5 +1,13 @@
-HOST = 'beaglebone.local'  # Beagle Bone Black hostname when connected to your LAN via ethernet
-PORT = 65432  # Port to listen on, this can be any arbitrary private port between 49152 and 65535
-ALLOW_SERVER_TIMEOUT = False  # implements server timeout
-SERVER_TIMEOUT = 20  # If no client connects within this timeframe (seconds), server closes.
-                     # Ignored if ALLOW_SERVER_TIMEOUT = False
+# BeagleBone Black IP address when connected to your LAN via ethernet
+# this can be any private IPv4 address but must be the same in both the
+# ace-test-framework code (run on the computer) and in the
+# ace-bbb-test-runner code (run on the BeagleBone)
+HOST = '10.16.132.250'
+# Port to listen on, this can be any arbitrary private port between
+# 49152 and 65535
+PORT = 65432
+# implements server timeout
+ALLOW_SERVER_TIMEOUT = False
+# If no client connects within this timeframe (seconds), server closes.
+# Ignored if ALLOW_SERVER_TIMEOUT = False
+SERVER_TIMEOUT = 20
